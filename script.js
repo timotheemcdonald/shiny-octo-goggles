b121b52647f3ae6b675f8f1100a3d37051e497cd
timotheemcdonald <timotheemcdonald@gmail.com>
2021-01-22 22:12:41 -0500

    added images

--- script.js
+++ script.js
@@ -1,2 +1,29 @@
 const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
+canvas.width = 800;
+canvas.height = 500;
+
+const keys = [];
+
+const player = {
+    x: 0,
+    y: 0,
+    width: 40,
+    height: 72,
+    frameX: 0,
+    frameY: 0,
+    speed: 9,
+    moving: false
+};
+
+const playerSprite = new Image();
+playerSprite.src = "chewie.png";
+
+const background = new Image();
+background.src = "background.png";
+
+function animate(){
+    ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+    requestAnimationFrame(animate);
+}
+animate();
