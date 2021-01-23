2133cb74fc750e4039f088bbec31933ee97dd5b0
timotheemcdonald <timotheemcdonald@gmail.com>
2021-01-22 22:58:51 -0500

    added fps

--- script.js
+++ script.js
@@ -26,39 +26,74 @@ function drawSprite(img, sX, sY, sW, sH, dX, dY, dW, dH){
 
 }
 
-function animate(){
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
-    drawSprite(playerSprite, player.width * player.frameX, player.height * player.frameY, player.width, player.height, player.x, player.y, player.width, player.height);
-    movePlayer();
-    requestAnimationFrame(animate);
-}
-animate();
-
 window.addEventListener("keydown", function(e){
     keys[e.key] = true;
-    console.log(e.key)
+    player.moving = true;
 });
 
 window.addEventListener("keyup", function(e){
-    delete keys[e.key]
+    delete keys[e.key];
+    player.moving = false;
 });
 
 function movePlayer(){
     if(keys['ArrowUp'] && player.y > 100){
         player.y -= player.speed;
         player.frameY = 3;
+        player.moving = true;
     }
     if(keys['ArrowLeft'] && player.x > 0 ){
         player.x -= player.speed;
         player.frameY = 1;
+        player.moving = true;
     }
     if(keys['ArrowDown'] && player.y < canvas.height - player.height){
         player.y += player.speed;
         player.frameY = 0;
+        player.moving = true;
     }
     if(keys['ArrowRight'] && player.x < canvas.width - player.width){
         player.x += player.speed;
         player.frameY = 2;
+        player.moving = true;
     }
 }
+
+function handlePlayerFrame(){
+    if (player.frameX < 3 && player.moving) player.frameX++
+    else player.frameX = 0
+}
+
+// function animate(){
+    // ctx.clearRect(0, 0, canvas.width, canvas.height);
+    // ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+    // drawSprite(playerSprite, player.width * player.frameX, player.height * player.frameY, player.width, player.height, player.x, player.y, player.width, player.height);
+    // movePlayer();
+    // handlePlayerFrame();
+    // requestAnimationFrame(animate);
+// }
+// animate();
+
+let fps, fpsInterval, startTime, now, then, elapsed;
+
+function startAnimating(fps){
+    fpsInterval = 1000/fps;
+    then = Date.now();
+    startTime = then;
+    animate();
+}
+
+function animate(){
+    requestAnimationFrame(animate);
+    now = Date.now();
+    elapsed = now - then;
+    if(elapsed > fpsInterval){
+        then = now - (elapsed % fpsInterval);
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+        drawSprite(playerSprite, player.width * player.frameX, player.height * player.frameY, player.width, player.height, player.x, player.y, player.width, player.height);
+        movePlayer();
+        handlePlayerFrame();
+    }
+}
+startAnimating(20);
