39a3fd1dede2bcab24ebeaacc0c70f5f1279b67f
timotheemcdonald <timotheemcdonald@gmail.com>
2021-01-22 22:22:33 -0500

    drawing sprite to screen

--- script.js
+++ script.js
@@ -18,12 +18,18 @@ const player = {
 
 const playerSprite = new Image();
 playerSprite.src = "chewie.png";
-
 const background = new Image();
 background.src = "background.png";
 
+function drawSprite(img, sX, sY, sW, sH, dX, dY, dW, dH){
+    ctx.drawImage(img, sX, sY, sW, sH, dX, dY, dW, dH);
+
+}
+
 function animate(){
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+    drawSprite(playerSprite, 0, 0, player.width, player.height, 200, 200, player.width, player.height);
     requestAnimationFrame(animate);
 }
 animate();
